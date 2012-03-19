0051495f268c314ca93e02bdf972576ced6d9a94
Alistair MacLeod <alistair@biscuitsfruit.org.uk>
2012-03-19 16:26:20 +0000

    Added atom feed for each tag. Added an index page of all tags.

--- _plugins/_tag_page_generator.rb
+++ _plugins/_tag_page_generator.rb
@@ -1,5 +1,5 @@
 module Jekyll
-  class TagIndex < Page
+  class TagPage < Page
     def initialize(site, base, dir, tag)
       @site = site
       @base = base
@@ -13,6 +13,35 @@ module Jekyll
       self.data['title'] = "#{tag_title_prefix}#{tag}#{tag_title_suffix}"
     end
   end
+
+  class TagPageAtom < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.xml'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag_page_atom.xml')
+      self.data['tag'] = tag
+      tag_title_prefix = site.config['tag_title_prefix'] || 'Posts Tagged &ldquo;'
+      tag_title_suffix = site.config['tag_title_suffix'] || '&rdquo;'
+      self.data['title'] = "#{tag_title_prefix}#{tag}#{tag_title_suffix}"
+    end
+  end
+
+  class TagIndex < Page
+    def initialize(site, base, dir, tags)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
+      self.data['tags'] = tags
+      self.data['title'] = "All Tags"
+    end
+  end
+
   class TagGenerator < Generator
     safe true
     def generate(site)
@@ -20,15 +49,45 @@ module Jekyll
       if site.layouts.key? 'tag_page'
         dir = site.config['tag_dir'] || 'blog/tag'
         site.tags.keys.each do |tag|
-          write_tag_index(site, File.join(dir, tag), tag)
+          write_tag_page(site, File.join(dir, tag), tag)
+          write_tag_page_atom(site, File.join(dir, tag), tag)
         end
       end
+
+      tags = Hash.new
+       for post in site.posts
+         for tag in post.tags
+           if tags[tag]
+             tags[tag] += 1
+           else
+             tags[tag] = 1
+           end
+         end
+       end
+
+      write_tag_index(site, dir, tags)
     end
-    def write_tag_index(site, dir, tag)
-      index = TagIndex.new(site, site.source, dir, tag)
+
+    def write_tag_index(site, dir, tags)
+      index = TagIndex.new(site, site.source, dir, tags)
       index.render(site.layouts, site.site_payload)
       index.write(site.dest)
       site.pages << index
     end
+
+    def write_tag_page(site, dir, tag)
+      tag_page = TagPage.new(site, site.source, dir, tag)
+      tag_page.render(site.layouts, site.site_payload)
+      tag_page.write(site.dest)
+      site.pages << tag_page
+    end
+
+    def write_tag_page_atom(site, dir, tag)
+      tag_page = TagPageAtom.new(site, site.source, dir, tag)
+      tag_page.render(site.layouts, site.site_payload)
+      tag_page.write(site.dest)
+      site.pages << tag_page
+    end
+
   end
 end
