559e84cbf91fadbbb8786c553be46be0de2439ba
Alistair MacLeod <alistair@biscuitsfruit.org.uk>
2013-08-30 10:06:52 -0500

    Added map of geotagged blog posts to main page.

---
+++ geotagged-blog-posts.js
@@ -0,0 +1,6 @@
+---
+---
+var geotagged_posts = [
+    {% for post in site.posts %}{% if post.location %}{latlon: "{{post.location}}", title: "{{post.title}}", url: "{{post.url}}"},
+{% endif %}{% endfor %}
+];
