4fecf5ed65186b02d14bed0fa2de5fe3bd3ffbd2
River MacLeod <river@mm0hai.net>
2016-12-15 20:35:59 +0000

    Escape quotes in post titles for javascript geotag data.

--- geotagged-blog-posts.js
+++ geotagged-blog-posts.js
@@ -1,6 +1,6 @@
 ---
 ---
 var geotagged_posts = [
-{% for post in site.posts %}{% if post.location %}{  latlon: "{{post.location}}", title: "{{post.title}}", url: "{{post.url}}"},
+{% for post in site.posts %}{% if post.location %}{  latlon: "{{post.location}}", title: "{{post.title | replace:'"','\"'"}}", url: "{{post.url}}"},
 {% endif %}{% endfor %}
 ];
